19cd1e79e6699dedb2fa13a3c087b6a2d8c4cda1
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-04-26 21:22:35 +0900

    Add standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly http = inject(HttpClient); // ?
+
+  users: User[] = [];
   title = 'angular-after-tutorial';
+
+
+  ngOnInit(): void {
+    this.http.get<{ data: User[] }>('https://reqres.in/api/users')
+    .subscribe(resp => this.users = resp.data);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()],
+}).catch(err => console.error(err));
